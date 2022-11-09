8e42518f3479010f42ea9c1b094f80fed2778422
Oleksandr Udovenko <107849168+Aleksudv@users.noreply.github.com>
2022-11-09 10:42:19 +0200

    onovlennya1

--- js/02-video.js
+++
@@ -1,3 +0,0 @@
-import Player from '@vimeo/player';
-
-console.log(Player);
--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,51 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+
+
+
+const galleryEl = document.querySelector(".js-gallery");
+
+const markup = galleryItems.map(({
+    preview,
+    original,
+    description
+}) => `<a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image js-galleryImg"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+  </a>`).join('');
+
+galleryEl.insertAdjacentHTML('beforeend', markup);
+
+
+const onGallaryImgClick = evt => {
+    evt.preventDefault();
+    const { target } = evt;
+    const largeImage = target.dataset.source;
+       
+    const instance = basicLightbox.create(`
+    <img src="${target.dataset.source}" width="800" height="600">
+`)
+    instance.show()
+    
+    console.log(largeImage);
+};
+galleryEl.addEventListener('click', onGallaryImgClick);
+
+
+const lightbox = new SimpleLightbox('.js-gallery a', {
+    captionsData: "alt",
+    captionsDelay: 250,
+    overlayOpacity:0.9,
+});
